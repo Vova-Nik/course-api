90059de0dbacaf5a818d80ac81d8d172df9259c3
Vova-Nik <33543617+Vova-Nik@users.noreply.github.com>
2020-03-31 16:59:17 +0300

    for push

--- src/main/java/io/javabrains/springbootquickstart/courseapi/JavaBrainsCourseApiApplication.java
+++ src/main/java/io/javabrains/springbootquickstart/courseapi/JavaBrainsCourseApiApplication.java
@@ -9,5 +9,5 @@ public class JavaBrainsCourseApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBrainsCourseApiApplication.class, args);
 	}
-
+//just for git
 }
