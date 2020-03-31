957a494ce103e29446ad21506adb6d7805965fde
Volodymyr Nikolenko <33543617+Vova-Nik@users.noreply.github.com>
2020-03-31 15:33:29 +0300

    Trying to comit firs

--- src/main/java/io/javabrainsspringbootquickstart/courseapi/JavaBrainsCourseApiApplication.java
+++ src/main/java/io/javabrainsspringbootquickstart/courseapi/JavaBrainsCourseApiApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaBrainsCourseApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JavaBrainsCourseApiApplication.class, args);
-	}
-
+public class CourseApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiApp.class, args);
+    }
 }
